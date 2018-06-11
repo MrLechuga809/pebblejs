195827d5127c42ec6dff8770b978c28741e796ee
MrLechuga809 <e.leonardo7474@gmail.com>
2018-06-11 17:33:41 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,27 +1,5 @@
 
-function httpGet(theUrl)
-{
-    if (window.XMLHttpRequest)
-    {// code for IE7+, Firefox, Chrome, Opera, Safari
-        xmlhttp=new XMLHttpRequest();
-    }
-    else
-    {// code for IE6, IE5
-        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-    }
-    xmlhttp.onreadystatechange=function()
-    {
-        if (xmlhttp.readyState==4 && xmlhttp.status==200)
-        {
-            return xmlhttp.responseText;
-        }
-    }
-    xmlhttp.open("GET", theUrl, false );
-    xmlhttp.send();    
-}
-var theUrl = "https://www.packtpub.com/packt/offers/free-learning";
-console.log(httpGet(theUrl));
-/* global simply 
+/* global simply */
 simply.text({
     title: 'Hello!',
     //subtitle: 'How are you?',
@@ -29,6 +7,6 @@ simply.text({
 }, true);
 
 simply.on('singleClick', function() {
-    var theUrl = "https://www.packtpub.com/packt/offers/free-learning";
-   simply.body(httpGet(theUrl)); 
-});*/
+    Pebble.showSimpleNotificationOnPebble('Hi Eddy!',
+  'Notifications from JavaScript? Welcome to the future!');
+});
