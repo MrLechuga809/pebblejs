e15bd7c2d38e8668d36d590a5bc849e548b035b6
MrLechuga809 <e.leonardo7474@gmail.com>
2018-06-11 17:21:21 -0400

    try somthng

--- test.js
+++ test.js
@@ -1,5 +1,25 @@
-/* global simply */
+var txt = "";
+function loadXMLDoc() {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        myFunction(this);
+      }
+    };
+    xmlhttp.open("GET", "https://www.packtpub.com/packt/offers/free-learning", true);
+    xmlhttp.send();
+  }
+  
+  function myFunction(xml) {
+    var x, i, xmlDoc;
+    xmlDoc = xml.responseXML;
+    
+    x = xmlDoc.getElementsByClassName("dotd-title");
 
+    txt = x[0].innerHTML;
+  }
+
+/* global simply */
 simply.text({
     title: 'Hello!',
     //subtitle: 'How are you?',
@@ -7,5 +27,5 @@ simply.text({
 }, true);
 
 simply.on('singleClick', function() {
-   simply.body("I'm doing great!"); 
+   simply.body(String.toString(txt)); 
 });
