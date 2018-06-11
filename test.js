8cdbd7d0e48ebf41908420ea9d65285370294f7e
MrLechuga809 <e.leonardo7474@gmail.com>
2018-06-11 17:51:02 -0400

    Update test.js

--- test.js
+++ test.js
@@ -7,6 +7,10 @@ simply.text({
 }, true);
 
 simply.on('singleClick', function() {
-    Pebble.showSimpleNotificationOnPebble('Hi Eddy!',
-  'Notifications from JavaScript? Welcome to the future!');
+    //Pebble.showSimpleNotificationOnPebble('Hi Eddy!',
+ // 'Notifications from JavaScript? Welcome to the future!');
+    var theUrl = 'https://www.packtpub.com/packt/offers/free-learning';
+  ajax({ url: theUrl, type: 'html' }, function(data) {
+    simply.text({ body: data.getElementsByClassName("dotd-title")[0].innerText });
+  });
 });
