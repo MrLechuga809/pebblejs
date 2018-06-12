bbb9c7f675a88903ea7a1fd8c976ffbcb47fb340
MrLechuga809 <e.leonardo7474@gmail.com>
2018-06-11 20:43:09 -0400

    Update test.js

--- test.js
+++ test.js
@@ -5,25 +5,14 @@ simply.text({
     //subtitle: 'How are you?',
     body:   'blablablablablabla',
 }, true);
-var theUrl = 'https://www.packtpub.com/packt/offers/free-learning';
 
-function makeHttpObject() {
-    try {return new XMLHttpRequest();}
-    catch (error) {}
-    try {return new ActiveXObject("Msxml2.XMLHTTP");}
-    catch (error) {}
-    try {return new ActiveXObject("Microsoft.XMLHTTP");}
-    catch (error) {}
-  
-    throw new Error("Could not create HTTP request object.");
-  }
-  var request = makeHttpObject();
-  request.open("GET", "theUrl", true);
-  request.send(null);
-  request.onreadystatechange = function() {
-    if (request.readyState == 4)
-      var parse = new DOMParser();
+
+ajax({ url: 'https://www.packtpub.com/packt/offers/free-learning' }, function(data){
+  var parse = new DOMParser();
       var html = parse.parseFromString(request.responseText, "html/text");
         Pebble.showSimpleNotificationOnPebble('Hello!', html.getElementsByClassName("dotd-title")[0].innerText);
-  };
+  simply.title(headline);
+});
+
+
 
